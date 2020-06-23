b7865dc971fc6490629eb2a8ab151ef3d94c419e
i5heu <i5h@i5h.eu>
2020-06-23 23:05:16 +0200

    add pseudo test

--- wikinota_backend/src/main.rs
+++ wikinota_backend/src/main.rs
@@ -5,3 +5,8 @@ fn main() {
     let boo: String = ["hi", &(FOO.to_string())].join(" ");
     println!("{}", boo.to_string());
 }
+
+#[test]
+fn it_works() {
+    assert_eq!(gerasdb::math::add(2, 2), 4);
+}
