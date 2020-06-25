92c9d4298ebfb0a037ddb78133caecb867c07682
i5heu <i5h@i5h.eu>
2020-06-25 11:10:50 +0200

    test sqlite of GerasDB

--- wikinota_backend/src/main.rs
+++ wikinota_backend/src/main.rs
@@ -1,12 +1,16 @@
 extern crate gerasdb;
 
 fn main() {
-    const FOO: i8 = gerasdb::math::add(2, 2);
-    let boo: String = ["hi", &(FOO.to_string())].join(" ");
-    println!("{}", boo.to_string());
+    // const FOO: i8 = gerasdb::math::add(2, 2);
+    // let boo: String = ["hi", &(FOO.to_string())].join(" ");
+    //     println!("{}", boo.to_string());
+    println!("hello world");
 }
 
 #[test]
-fn it_works() {
-    assert_eq!(gerasdb::math::add(2, 2), 4);
+fn it_works() -> Result<(), gerasdb::dbError> {
+    let result = gerasdb::init()?;
+    // assert_eq!(gerasdb::math::add(2, 2), "Hello");
+
+    Ok(())
 }
